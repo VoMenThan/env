b1710477d2d0b972698cdffbd5334ce2d5d1d48f
than vo <thancn12@gmail.com>
2019-03-04 14:39:44 +0700

    update services 4/3

--- wp-content/themes/envzone/inc/full-cycle-development.php
+++ wp-content/themes/envzone/inc/full-cycle-development.php
@@ -127,6 +127,77 @@
         </div>
     </section>
 
-    <?php require_once "why-business-services.php";?>
+    <!--WHY BUSINESS-->
+    <div class="box-why-business why-industries">
+        <img class="img-industries img-fluid" src="<?php echo ASSET_URL;?>images/img-bg-full-cycle-development-services.png" alt="">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 text-lg-center text-left">
+                    <h2>
+                        WE MAKE TEAM LEADERS FREE UP THEIR THEIR MIND
+                    </h2>
+                    <div class="description-business">
+                        A premium outsourcing solution for minimum management and maximum output
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-6 col-mbx-100 mb-lg-0 mb-3">
+                    <div class="item-reason">
+                        <i class="fa fa-check-circle"></i>
+                        <p>
+                            We have dedicated partners who make it their business to know your product and services 
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-6 col-mbx-100 mb-lg-0 mb-3">
+                    <div class="item-reason">
+                        <i class="fa fa-check-circle"></i>
+                        <p>
+                            Adjustment to your resources to provide your needs comes easily to us
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-6 col-mbx-100 mb-lg-0 mb-3">
+                    <div class="item-reason">
+                        <i class="fa fa-check-circle"></i>
+                        <p>
+
+                            We provide a trusted solution to manage overhead costs of handling your business
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-6 col-mbx-100 mb-lg-0 mb-3">
+                    <div class="item-reason">
+                        <i class="fa fa-check-circle"></i>
+                        <p>
+                            Our results are focused on your business goals and values.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-6 mb-lg-0 mb-3">
+                    <div class="box-dedicated">
+                        <h4>Dedicated Teams</h4>
+                        <p>
+                            EnvZone will provide you with the opportunity of working with offshore teams dedicated to serving you.
+                        </p>
+                    </div>
+
+                </div>
+                <div class="col-lg-5 col-md-6 mb-lg-0 mb-3">
+                    <div class="box-dedicated">
+                        <h4>Pool of Talent</h4>
+                        <p>
+                            We have a pool of partners who have the capability of working with an array of industries and build a team that is tailored to your business needs and has all the tools to give you results.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <a href="<?php echo get_home_url();?>/contact-us" class="btn btn-green-env">Schedule an Appointment Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--END WHY BUSINESS-->
     <?php require_once "subscribe.php";?>
 </main>
